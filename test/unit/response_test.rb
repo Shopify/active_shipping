82b2a71e740b5acb87d75f6d7b958cc47bec42ab
Cody Fauser <codyfauser@gmail.com>
2009-03-03 11:16:36 -0500

    Remove unused code from response test

--- test/unit/response_test.rb
+++ test/unit/response_test.rb
@@ -1,17 +1,9 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ResponseTest < Test::Unit::TestCase
-  include ActiveMerchant::Shipping
-  
-  
-  def setup
-
-  end
-
   def test_initialize
-    response = nil
     assert_nothing_raised do
-      response = RateResponse.new(true, "success!", {:rate => 'Free!'}, :rates => [stub(:service_name => 'Free!', :total_price => 0)], :xml => "<rate>Free!</rate>")
+      RateResponse.new(true, "success!", {:rate => 'Free!'}, :rates => [stub(:service_name => 'Free!', :total_price => 0)], :xml => "<rate>Free!</rate>")
     end
     
   end
