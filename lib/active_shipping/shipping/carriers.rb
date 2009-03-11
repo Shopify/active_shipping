52775bea66c913e9f038f808640c647681bcf72e
Cody Fauser <codyfauser@gmail.com>
2009-03-10 23:57:38 -0400

    Add support for Shipwire Shipping Rate API

--- lib/active_shipping/shipping/carriers.rb
+++ lib/active_shipping/shipping/carriers.rb
@@ -2,6 +2,7 @@ require 'active_shipping/shipping/carriers/bogus_carrier'
 require 'active_shipping/shipping/carriers/ups'
 require 'active_shipping/shipping/carriers/usps'
 require 'active_shipping/shipping/carriers/fedex'
+require 'active_shipping/shipping/carriers/shipwire'
 
 module ActiveMerchant
   module Shipping
