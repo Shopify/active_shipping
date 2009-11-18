bf7b1e0c17569b392714b7028ec6a2d555dc70c6
James MacAulay <james@jadedpixel.com>
2009-11-18 17:54:12 -0500

    improve Base#carrier lookup method

--- lib/active_shipping/shipping/base.rb
+++ lib/active_shipping/shipping/base.rb
@@ -3,12 +3,9 @@ module ActiveMerchant
     module Base
       mattr_accessor :mode
       self.mode = :production
-      
-      ALLCAPS_NAMES = ['usps','dhl'] # is the class name allcaps like USPS or camelcase like FedEx?
-      
+
       def self.carrier(name)
-        name = name.to_s.downcase
-        ActiveMerchant::Shipping.const_get(ALLCAPS_NAMES.include?(name) ? name.upcase : name.camelize)
+        ActiveMerchant::Shipping::Carriers.all.find {|c| c.name.downcase == name}
       end
     end
   end
