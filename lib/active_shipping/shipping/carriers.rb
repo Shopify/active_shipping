96440c68d5fa11e25f221723e0ecb9656ab02d0a
Cody Fauser <codyfauser@gmail.com>
2010-01-30 01:15:20 -0500

    Add support for Kunaki rate service

--- lib/active_shipping/shipping/carriers.rb
+++ lib/active_shipping/shipping/carriers.rb
@@ -3,13 +3,14 @@ require 'active_shipping/shipping/carriers/ups'
 require 'active_shipping/shipping/carriers/usps'
 require 'active_shipping/shipping/carriers/fedex'
 require 'active_shipping/shipping/carriers/shipwire'
+require 'active_shipping/shipping/carriers/kunaki'
 
 module ActiveMerchant
   module Shipping
     module Carriers
       class <<self
         def all
-          [BogusCarrier, UPS, USPS, FedEx, Shipwire]
+          [BogusCarrier, UPS, USPS, FedEx, Shipwire, Kunaki]
         end
       end
     end
