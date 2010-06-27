81d94f0f36c37fdfbc0eb2e53e3660b43d0a4c5a
Tobias Lütke <tobi@shopify.com>
2010-06-28 06:11:18 +0800

    Moved ActiveMerchant common things to a common include

--- lib/active_shipping.rb
+++ lib/active_shipping.rb
@@ -37,11 +37,8 @@ require 'quantified/mass'
 require 'quantified/length'
 
 require 'net/https'
-require 'active_shipping/lib/error'
-require 'active_shipping/lib/requires_parameters'
-require 'active_shipping/lib/connection'
-require 'active_shipping/lib/posts_data'
-require 'active_shipping/lib/country'
+
+require 'active_shipping/common'
 
 require 'active_shipping/shipping/base'
 require 'active_shipping/shipping/response'
