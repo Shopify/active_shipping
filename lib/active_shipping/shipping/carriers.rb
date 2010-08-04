a9b3fde03449a932948ac47ba9ccb10c18ea2fb3
William Lang <william@shopify.com>
2010-08-04 16:38:48 -0400

    Added CanadaPost as a Carrier

--- lib/active_shipping/shipping/carriers.rb
+++ lib/active_shipping/shipping/carriers.rb
@@ -3,13 +3,14 @@ require 'active_shipping/shipping/carriers/usps'
 require 'active_shipping/shipping/carriers/fedex'
 require 'active_shipping/shipping/carriers/shipwire'
 require 'active_shipping/shipping/carriers/kunaki'
+require 'active_shipping/shipping/carriers/canada_post'
 
 module ActiveMerchant
   module Shipping
     module Carriers
       class <<self
         def all
-          [BogusCarrier, USPS, FedEx, Shipwire, Kunaki]
+          [BogusCarrier, USPS, FedEx, Shipwire, Kunaki, CanadaPost]
         end
       end
     end
