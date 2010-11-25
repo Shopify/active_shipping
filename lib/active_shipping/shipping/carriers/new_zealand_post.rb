c5c882c6602ab2829ded06e46684d1a3096565a8
Cameron Fowler <cameron.fowler@abletech.co.nz>
2010-11-25 17:11:56 +1300

    more tests for parsing responses

--- lib/active_shipping/shipping/carriers/new_zealand_post.rb
+++ lib/active_shipping/shipping/carriers/new_zealand_post.rb
@@ -4,6 +4,9 @@ module ActiveMerchant
   module Shipping
     class NewZealandPost < Carrier
 
+      class NewZealandPostRateResponse < RateResponse
+      end
+
       URL = "http://workshop.nzpost.co.nz/api/v1/rate.xml"
 
       # Override to return required keys in options hash for initialize method.
@@ -34,19 +37,19 @@ module ActiveMerchant
                       :zip => '6012',
                       :phone => '')
       end
-      
+
       private
-      
+
       def build_rectangular_request_params(origin, destination, line_items = [], options = {})
         params = {
           :postcode_src => origin[:postal_code],
           :postcode_dest => destination[:postal_code],
           :api_key => @options[:api_key]
         }
-        
+
         combine_line_items(line_items).merge(params)
       end
-      
+
       def combine_line_items(line_items)
         {
           :height => line_items.first.centimetres(:height).to_s,
@@ -55,6 +58,28 @@ module ActiveMerchant
           :weight =>"%.1f" % (line_items.first.weight.amount / 1000.0)
         }
       end
+
+      def parse_rate_response(origin, destination, packages, response, options={})
+        # xml = REXML::Document.new(response)
+        # if success = response_success?(xml)
+        #   rate_estimates = []
+        #   xml.elements.each('hash/products-by-service') do ||
+        #   end
+        # end
+      end
+
+      def response_success?(xml)
+        xml.get_text('hash/status').to_s == 'success'
+      end
+
+      def response_message(xml)
+        if response_success?(xml)
+          'Success'
+        else
+          xml.get_text('hash/message').to_s
+        end
+      end
+
     end
   end
 end
