0bd28e6f2f6e663f47b6f7df254ed754794fabf1
Dennis Theisen <dennis@shopify.com>
2011-01-05 15:14:08 +0100

    Shipwire: Send the address name as well if provided

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -161,8 +161,16 @@ module ActiveMerchant
                                       :state => 'NY',
                                       :address1 => '780 3rd Avenue',
                                       :address2 => 'Suite  2601',
-                                      :zip => '10017')
+                                      :zip => '10017'),
                                       
+        :new_york_with_name => Location.new(
+                                      :name => "Bob Bobsen",
+                                      :country => 'US',
+                                      :city => 'New York',
+                                      :state => 'NY',
+                                      :address1 => '780 3rd Avenue',
+                                      :address2 => 'Suite  2601',
+                                      :zip => '10017')
       }
       
     end
