c4d6a413470b126bb644ac8fb8fbce957b5350e9
Denis Odorcic <denis.odorcic@jadedpixel.com>
2011-08-19 15:05:03 -0400

    Add Gemfile, use Bundler::GemHelper

--- lib/vendor/test_helper.rb
+++ lib/vendor/test_helper.rb
@@ -4,10 +4,4 @@ $:.unshift(File.dirname(__FILE__) + '/../lib')
 
 require 'test/unit'
 require 'active_shipping'
-
-begin
-  require 'mocha'
-rescue LoadError
-  require 'rubygems'
-  require 'mocha'
-end
+require 'mocha'
