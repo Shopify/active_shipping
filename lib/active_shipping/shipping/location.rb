63a4670dc230f0082b9a62f0f956cb9d8e0af675
Jeff Browning <jeff@jkbrowning.com>
2011-12-12 12:23:58 -0600

    added zip_plus_4 zip code formatting to Location

--- lib/active_shipping/shipping/location.rb
+++ lib/active_shipping/shipping/location.rb
@@ -131,6 +131,18 @@ module ActiveMerchant #:nodoc:
         string << "\nFax: #{@fax}" unless @fax.blank?
         string
       end
+
+      # Returns the postal code as a properly formatted Zip+4 code, e.g. "77095-2233"
+      def zip_plus_4
+        if /(?<zip>\d{5})(?<plus_4>\d{4})/ =~ @postal_code
+          return "#{zip}-#{plus_4}"
+        elsif /\d{5}-\d{4}/ =~ @postal_code
+          return @postal_code
+        else
+          nil
+        end
+      end
+
     end
       
   end
