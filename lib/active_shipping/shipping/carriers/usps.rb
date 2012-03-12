1b13e0c3bdb3ff3949360eebc38b11eba30f158b
Dan Buettner <dbuettner@merchantsbonding.com>
2012-03-11 21:25:48 -0500

    first group of changes for USPS tracking
    Start of adding USPS tracking capability
    
    * Currently using FedEx sample doc; needs replaced
    * Unit only - not remote yet
    
    passing unit tests for USPS tracking
    
    add USPS timestamp tracking info
    
    further development of USPS tracking
    
    USPS tracking response working
    
    To-do: parse tracking events for location, datetime, message
    
    Cleanup, additional testing, parsing of more USPS data
    
    Handle package not found in the test environment
    
    general cleanup prior to pull request
    
    more general cleanup prior to pull request
    
    further cleanup prior to pull request
    
    Address date-format unit test failure in 1.8.7
    
    Add carrier name to USPS tracking response

--- lib/active_shipping/shipping/carriers/usps.rb
+++ lib/active_shipping/shipping/carriers/usps.rb
@@ -31,12 +31,14 @@ module ActiveMerchant
       API_CODES = {
         :us_rates => 'RateV4',
         :world_rates => 'IntlRateV2',
-        :test => 'CarrierPickupAvailability'
+        :test => 'CarrierPickupAvailability',
+        :track => 'TrackV2'
       }
       USE_SSL = {
         :us_rates => false,
         :world_rates => false,
-        :test => true
+        :test => true,
+        :track => false
       }
       CONTAINERS = {
         :envelope => 'Flat Rate Envelope',
@@ -48,6 +50,7 @@ module ActiveMerchant
         :matter_for_the_blind => 'Matter for the blind',
         :envelope => 'Envelope'
       }
+
       PACKAGE_PROPERTIES = {
         'ZipOrigination' => :origin_zip,
         'ZipDestination' => :destination_zip,
@@ -122,6 +125,13 @@ module ActiveMerchant
         "WS" => "Western Samoa"
       }
 
+      def find_tracking_info(tracking_number, options={})
+        options = @options.update(options)        
+        tracking_request = build_tracking_request(tracking_number, options)
+        response = commit(:track, tracking_request, (options[:test] || false))
+        parse_tracking_response(response, options)
+      end
+
       def self.size_code_for(package)
         if package.inches(:max) <= 12
           'REGULAR'
@@ -178,8 +188,99 @@ module ActiveMerchant
         Mass.new(70, :pounds)
       end
       
-      protected
       
+      def find_tracking_info(tracking_number, options={})
+        options = @options.update(options)
+        tracking_request = build_tracking_request(tracking_number, options)
+        response = commit(save_request(tracking_request), (options[:test] || false)).gsub(/<(\/)?.*?\:(.*?)>/, '<\1\2>')
+        parse_tracking_response(response, options)
+      end
+      
+
+      protected
+
+
+
+      def response_success?(xml)
+        xml.get_text('/*/Response/ResponseStatusCode').to_s == '1'
+      end
+
+      def response_message(xml)
+        xml.get_text('/*/Response/Error/ErrorDescription | /*/Response/ResponseStatusDescription').to_s
+      end
+
+      def parse_tracking_response(response, options={})
+        xml = REXML::Document.new(response)
+        success = response_success?(xml)
+        message = response_message(xml)
+        
+        if success
+          tracking_number, origin, destination = nil
+          shipment_events = []
+          
+          first_shipment = xml.elements['/*/Shipment']
+          first_package = first_shipment.elements['Package']
+          tracking_number = first_shipment.get_text('ShipmentIdentificationNumber | Package/TrackingNumber').to_s
+          
+          origin, destination = %w{Shipper ShipTo}.map do |location|
+            location_from_address_node(first_shipment.elements["#{location}/Address"])
+          end
+          
+          activities = first_package.get_elements('Activity')
+          unless activities.empty?
+            shipment_events = activities.map do |activity|
+              description = activity.get_text('Status/StatusType/Description').to_s
+              zoneless_time = if (time = activity.get_text('Time')) &&
+                                 (date = activity.get_text('Date'))
+                time, date = time.to_s, date.to_s
+                hour, minute, second = time.scan(/\d{2}/)
+                year, month, day = date[0..3], date[4..5], date[6..7]
+                Time.utc(year, month, day, hour, minute, second)
+              end
+              location = location_from_address_node(activity.elements['ActivityLocation/Address'])
+              ShipmentEvent.new(description, zoneless_time, location)
+            end
+            
+            shipment_events = shipment_events.sort_by(&:time)
+            
+            if origin
+              first_event = shipment_events[0]
+              same_country = origin.country_code(:alpha2) == first_event.location.country_code(:alpha2)
+              same_or_blank_city = first_event.location.city.blank? or first_event.location.city == origin.city
+              origin_event = ShipmentEvent.new(first_event.name, first_event.time, origin)
+              if same_country and same_or_blank_city
+                shipment_events[0] = origin_event
+              else
+                shipment_events.unshift(origin_event)
+              end
+            end
+            if shipment_events.last.name.downcase == 'delivered'
+              shipment_events[-1] = ShipmentEvent.new(shipment_events.last.name, shipment_events.last.time, destination)
+            end
+          end
+          
+        end
+        TrackingResponse.new(success, message, Hash.from_xml(response).values.first,
+          :xml => response,
+          :request => last_request,
+          :shipment_events => shipment_events,
+          :origin => origin,
+          :destination => destination,
+          :tracking_number => tracking_number)
+      end
+
+
+
+
+
+
+      def build_tracking_request(tracking_number, options={})
+        xml_request = XmlNode.new('TrackRequest', 'USERID' => @options[:login]) do |root_node|
+          root_node << XmlNode.new('TrackID', :ID => tracking_number)
+        end
+        URI.encode(xml_request.to_s)
+      end
+
       def us_rates(origin, destination, packages, options={})
         request = build_us_rate_request(packages, origin.zip, destination.zip, options)
          # never use test mode; rate requests just won't work on test servers
@@ -443,6 +544,69 @@ module ActiveMerchant
                     package.inches(:length) + package.inches(:width) + package.inches(:height)))
 
         return valid
+      end
+
+      def parse_tracking_response(response, options)
+        xml = REXML::Document.new(response)
+        root_node = xml.elements['TrackResponse']
+        
+        success = response_success?(xml)
+        message = response_message(xml)
+        
+        if success
+          tracking_number, origin, destination = nil
+          shipment_events = []
+          tracking_details = xml.elements.collect('*/*/TrackDetail'){ |e| e }
+          
+          tracking_summary = xml.elements.collect('*/*/TrackSummary'){ |e| e }.first
+          tracking_details << tracking_summary
+          
+          tracking_number = root_node.elements['TrackInfo'].attributes['ID'].to_s
+
+          tracking_details.each do |event|
+            location = nil
+            timestamp = nil
+            description = nil
+            if event.get_text.to_s =~ /^(.*), (\w+ \d\d, \d{4}, \d{1,2}:\d\d [ap]m), (.*), (\w\w) (\d{5})$/i ||
+                event.get_text.to_s =~ /^Your item \w{2,3} (out for delivery|delivered) at (\d{1,2}:\d\d [ap]m on \w+ \d\d, \d{4}) in (.*), (\w\w) (\d{5})\.$/i
+              description = $1.upcase
+              timestamp   = $2
+              city        = $3
+              state       = $4
+              zip_code    = $5
+              location = Location.new(:city => city, :state => state, :postal_code => zip_code, :country => 'USA')
+            end
+            if location
+              time = Time.parse(timestamp)
+              zoneless_time = Time.utc(time.year, time.month, time.mday, time.hour, time.min, time.sec)
+              shipment_events << ShipmentEvent.new(description, zoneless_time, location)
+            end
+          end
+          shipment_events = shipment_events.sort_by(&:time)
+        end
+        
+        TrackingResponse.new(success, message, Hash.from_xml(response),
+          :carrier => @@name,
+          :xml => response,
+          :request => last_request,
+          :shipment_events => shipment_events,
+          :destination => destination,
+          :tracking_number => tracking_number
+        )
+      end
+            
+      def response_status_node(document)
+        document.elements['*/*/TrackSummary'] || document.elements['Error/Description']
+      end
+      
+      def response_success?(document)
+        summary = response_status_node(document).get_text.to_s
+        !(summary =~ /There is no record of that mail item/ || summary =~ /This Information has not been included in this Test Server\./)
+      end
+      
+      def response_message(document)
+        response_node = response_status_node(document)
+        response_status_node(document).get_text.to_s
       end
       
       def commit(action, request, test = false)
