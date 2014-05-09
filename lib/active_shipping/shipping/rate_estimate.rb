150dd735a2a3a15f10a91dbd8b647e8334c1ed60
Caleb Simpson <caleb.simpson@jadedpixel.com>
2014-05-09 17:57:48 +0000

    Standardize currencies to ISO when creating RateEstimates.

--- lib/active_shipping/shipping/rate_estimate.rb
+++ lib/active_shipping/shipping/rate_estimate.rb
@@ -26,7 +26,7 @@ module ActiveMerchant #:nodoc:
         end
         @total_price = Package.cents_from(options[:total_price])
         @negotiated_rate = options[:negotiated_rate] ? Package.cents_from(options[:negotiated_rate]) : nil
-        @currency = options[:currency]
+        @currency = ActiveMerchant::CurrencyCode.standardize(options[:currency])
         @delivery_range = options[:delivery_range] ? options[:delivery_range].map { |date| date_for(date) }.compact : []
         @shipping_date = date_for(options[:shipping_date])
         @delivery_date = @delivery_range.last
