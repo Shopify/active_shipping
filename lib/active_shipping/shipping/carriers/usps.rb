265c159482bfef3a200b22c234b332f9cfc9837c
Gary Mardell <gary@shopify.com>
2014-07-31 14:29:01 +0000

    Convert service to a symbol before use

--- lib/active_shipping/shipping/carriers/usps.rb
+++ lib/active_shipping/shipping/carriers/usps.rb
@@ -269,7 +269,7 @@ module ActiveMerchant
                 default_service = :all
               end
 
-              package << XmlNode.new('Service', US_SERVICES[options[:service] || default_service])
+              package << XmlNode.new('Service', US_SERVICES[options[:service].try(:to_sym) || default_service])
               package << XmlNode.new('FirstClassMailType', FIRST_CLASS_MAIL_TYPES[options[:first_class_mail_type]])
               package << XmlNode.new('ZipOrigination', strip_zip(origin_zip))
               package << XmlNode.new('ZipDestination', strip_zip(destination_zip))
