4d6a808c6a2f91e7a304d27897271923ac01b0ed
Blake Mesdag <blakemesdag@gmail.com>
2014-08-07 16:40:24 -0400

    Describe inches in the metric primitive unit so it initializes correctly

--- lib/vendor/quantified/lib/quantified/length.rb
+++ lib/vendor/quantified/lib/quantified/length.rb
@@ -2,16 +2,16 @@ module Quantified
   class Length < Attribute
     system :metric do
       primitive :metre
-      
+
       one :centimetre, :is => Length.new(0.01, :metres)
       one :millimetre, :is => Length.new(0.1, :centimetres)
       one :kilometre, :is => Length.new(1000, :metres)
     end
-    
+
     system :imperial do
       primitive :inch
-      one :inch, :is => Length.new(2.540, :centimetres)
-      
+      one :inch, :is => Length.new(0.0254, :metres)
+
       one :foot, :plural => :feet, :is => Length.new(12, :inches)
       one :yard, :is => Length.new(3, :feet)
       one :mile, :is => Length.new(5280, :feet)
