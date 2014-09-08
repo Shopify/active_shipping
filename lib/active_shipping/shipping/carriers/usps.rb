49d310728f5998ed90b79ea456b253e87f792d07
Dan Kubb <dan.kubb@gmail.com>
2014-09-08 13:52:32 -0700

    Extract default service into a private method

--- lib/active_shipping/shipping/carriers/usps.rb
+++ lib/active_shipping/shipping/carriers/usps.rb
@@ -274,14 +274,6 @@ module ActiveMerchant
         request = XmlNode.new('RateV4Request', :USERID => @options[:login]) do |rate_request|
           packages.each_with_index do |p,id|
             rate_request << XmlNode.new('Package', :ID => id.to_s) do |package|
-              default_service = if commercial_base?
-                :online
-              elsif commercial_plus?
-                :plus
-              else
-                :all
-              end
-
               service = options.fetch(:service, default_service).to_sym
 
               if commercial_type && service != default_service
@@ -628,6 +620,16 @@ module ActiveMerchant
           :plus
         elsif commercial_base?
           :base
+        end
+      end
+
+      def default_service
+        if commercial_base?
+          :online
+        elsif commercial_plus?
+          :plus
+        else
+          :all
         end
       end
 
