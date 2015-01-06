3013fac862b855ffaec31bba0aa799f55045e511
Willem van Bergen <willem@vanbergen.org>
2015-01-06 17:11:02 -0500

    Use autoload for carriers, so we don’t load them all if not needed.

--- lib/active_shipping/carriers.rb
+++ lib/active_shipping/carriers.rb
@@ -1,21 +1,29 @@
-require 'active_shipping/carriers/benchmark_carrier'
-require 'active_shipping/carriers/bogus_carrier'
-require 'active_shipping/carriers/ups'
-require 'active_shipping/carriers/usps'
-require 'active_shipping/carriers/fedex'
-require 'active_shipping/carriers/shipwire'
-require 'active_shipping/carriers/kunaki'
-require 'active_shipping/carriers/canada_post'
-require 'active_shipping/carriers/new_zealand_post'
-require 'active_shipping/carriers/canada_post_pws'
-require 'active_shipping/carriers/stamps'
-
 module ActiveShipping
   module Carriers
-    class <<self
-      def all
-        [BenchmarkCarrier, BogusCarrier, UPS, USPS, FedEx, Shipwire, Kunaki, CanadaPost, NewZealandPost, CanadaPostPWS, Stamps]
-      end
+    extend self
+
+    attr_reader :registered
+    @registered = []
+
+    def register(class_name, autoload_require)
+      ActiveShipping.autoload(class_name, autoload_require)
+      self.registered << class_name
+    end
+
+    def all
+      ActiveShipping::Carriers.registered.map { |name| ActiveShipping.const_get(name) }
     end
   end
 end
+
+ActiveShipping::Carriers.register :BenchmarkCarrier, 'active_shipping/carriers/benchmark_carrier'
+ActiveShipping::Carriers.register :BogusCarrier,     'active_shipping/carriers/bogus_carrier'
+ActiveShipping::Carriers.register :UPS,              'active_shipping/carriers/ups'
+ActiveShipping::Carriers.register :USPS,             'active_shipping/carriers/usps'
+ActiveShipping::Carriers.register :FedEx,            'active_shipping/carriers/fedex'
+ActiveShipping::Carriers.register :Shipwire,         'active_shipping/carriers/shipwire'
+ActiveShipping::Carriers.register :Kunaki,           'active_shipping/carriers/kunaki'
+ActiveShipping::Carriers.register :CanadaPost,       'active_shipping/carriers/canada_post'
+ActiveShipping::Carriers.register :NewZealandPost,   'active_shipping/carriers/new_zealand_post'
+ActiveShipping::Carriers.register :CanadaPostPWS,    'active_shipping/carriers/canada_post_pws'
+ActiveShipping::Carriers.register :Stamps,           'active_shipping/carriers/stamps'
