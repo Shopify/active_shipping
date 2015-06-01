d21632fff044ab6c15e7da2f0b4607af70c0f4aa
Tristan Hume <tristan.hume@shopify.com>
2015-06-01 16:34:18 -0400

    Give dates in UTC
    Signed-off-by: Tristan Hume <tristan.hume@shopify.com>

--- lib/active_shipping/carriers/usps.rb
+++ lib/active_shipping/carriers/usps.rb
@@ -404,7 +404,7 @@ module ActiveShipping
                 xml.public_send(COMMERCIAL_FLAG_NAME.fetch(commercial_type), 'Y')
               end
               xml.OriginZip(origin.zip)
-              xml.AcceptanceDateTime(DateTime.now.iso8601)
+              xml.AcceptanceDateTime(Time.now.utc.iso8601)
               xml.DestinationPostalCode(destination.zip)
             end
           end
