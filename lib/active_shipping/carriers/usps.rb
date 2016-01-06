de507016067e9c87f7105dd1436d3027c0c6552c
Chris Wise <cwise@murmurinformatics.com>
2016-01-06 11:27:50 -0500

    added support for USPS attempted delivery dates

--- lib/active_shipping/carriers/usps.rb
+++ lib/active_shipping/carriers/usps.rb
@@ -630,6 +630,9 @@ module ActiveShipping
 
         shipment_events = shipment_events.sort_by(&:time)
 
+        # USPS defines a delivery attempt with code 55
+        attempted_delivery_date = shipment_events.detect{ |shipment_event| shipment_event.type_code=="55" }.try(:time)
+
         if last_shipment = shipment_events.last
           status = last_shipment.status
           actual_delivery_date = last_shipment.time if last_shipment.delivered?
@@ -645,6 +648,7 @@ module ActiveShipping
                            :tracking_number => tracking_number,
                            :status => status,
                            :actual_delivery_date => actual_delivery_date,
+                           :attempted_delivery_date => attempted_delivery_date,
                            :scheduled_delivery_date => scheduled_delivery
       )
     end
