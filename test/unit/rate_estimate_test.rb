d8685d1ed1d438e4e163a77ddfe0ea5832dc1b03
Jonathan Kwok <jonathan.kwok@shopify.com>
2017-01-10 18:58:30 -0500

    Modernize RateEstimateTest

--- test/unit/rate_estimate_test.rb
+++ test/unit/rate_estimate_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class RateEstimateTest < ActiveSupport::TestCase
-  def setup
+  setup do
     @origin      = {address1: "61A York St", city: "Ottawa", province: "ON", country: "Canada", postal_code: "K1N 5T2"}
     @destination = {city: "Beverly Hills", state: "CA", country: "United States", postal_code: "90210"}
     @line_items  = [Package.new(500, [2, 3, 4], description: "a box full of stuff", value: 2500)]
@@ -11,11 +11,7 @@ class RateEstimateTest < ActiveSupport::TestCase
     @rate_estimate = RateEstimate.new(@origin, @destination, @carrier, @service_name, @options)
   end
 
-  def test_date_for_nil_string
-    assert_nil @rate_estimate.send(:date_for, nil)
-  end
-
-  def test_phone_required
+  test "#initialize accepts phone_required option field" do
     est = RateEstimate.new(@origin, @destination, @carrier, @service_name, @options.merge(phone_required: true))
     assert_equal true, est.phone_required
 
@@ -26,44 +22,40 @@ class RateEstimateTest < ActiveSupport::TestCase
     assert_equal false, est.phone_required
   end
 
-  def test_accepts_description_field
+  test "#initialize accepts description option field" do
     rate_estimate = RateEstimate.new(@origin, @destination, @carrier, @service_name, @options.merge(description: "It's free!"))
     assert_equal "It's free!", rate_estimate.description
   end
 
-  def test_date_for_invalid_string_in_ruby_19
-    assert_nil @rate_estimate.send(:date_for, "Up to 2 weeks") if RUBY_VERSION.include?('1.9')
-  end
-
-  def test_rate_estimate_converts_noniso_to_iso
+  test "#initialize converts noniso currency to iso" do
     rate_estimate = RateEstimate.new(@origin, @destination, @carrier, @service_name, @options.merge(currency: 'UKL'))
     assert_equal 'GBP', rate_estimate.currency
   end
 
-  def test_creating_an_estimate_with_an_invalid_currency_raises
+  test "#initialize raises if invalid currency code" do
     assert_raises(ActiveUtils::InvalidCurrencyCodeError) do
       RateEstimate.new(nil, nil, nil, nil, currency: 'FAKE')
     end
   end
 
-  def test_estimate_reference_is_set
+  test "#initialize accepts estimate_reference option field" do
     est = RateEstimate.new(@origin, @destination, @carrier, @service_name, @options.merge(estimate_reference: "somefakeref"))
 
     assert_equal "somefakeref", est.estimate_reference
   end
 
-  def test_compare_price_is_set
+  test "#initialize accepts compare_price option field" do
     est = RateEstimate.new(@origin, @destination, @carrier, @service_name, @options.merge(compare_price: 10.0))
     assert_equal 1000, est.compare_price
   end
 
-  def test_delivery_category_is_set
+  test "#initialize accepts delivery_category option field" do
     est = RateEstimate.new(@origin, @destination, @carrier, @service_name, @options.merge(delivery_category: "local_delivery"))
 
     assert_equal "local_delivery", est.delivery_category
   end
 
-  def test_charge_items_is_set
+  test "#initialize accepts charge_items option field" do
     charge_items = [
       {
         group: "base_charge",
@@ -85,13 +77,17 @@ class RateEstimateTest < ActiveSupport::TestCase
     assert_equal charge_items, est.charge_items
   end
 
-  def test_delivery_date_pulls_from_delivery_range
+  test "delivery_date is pulled from the later date of the delivery_range" do
     assert_equal [DateTime.parse("Fri 01 Jul 2016"), DateTime.parse("Sun 03 Jul 2016")], @rate_estimate.delivery_range
     assert_equal DateTime.parse("Sun 03 Jul 2016"), @rate_estimate.delivery_date
   end
 
-  def test_messages_is_set
+  test "#initialize accepts messages option field" do
     rate = RateEstimate.new(@origin, @destination, @carrier, @service_name, @options.merge(messages: ["warning"]))
     assert_equal ["warning"], rate.messages
+  end
+
+  test "#date_for returns nil when given nil string" do
+    assert_nil @rate_estimate.send(:date_for, nil)
   end
 end
