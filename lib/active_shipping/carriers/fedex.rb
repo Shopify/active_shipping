7f90f6652676d5c33ac1147a2058478067b9f4c4
Jason Gedge <jason.gedge@shopify.com>
2017-01-23 14:04:04 -0500

    Raise a ResponseError for FedEx#find_tracking_info

--- lib/active_shipping/carriers/fedex.rb
+++ lib/active_shipping/carriers/fedex.rb
@@ -590,7 +590,6 @@ module ActiveShipping
       message = response_message(xml)
 
       if success
-        origin = nil
         delivery_signature = nil
         shipment_events = []
 
@@ -599,12 +598,27 @@ module ActiveShipping
           when 1
             all_tracking_details.first
           when 0
-            raise ActiveShipping::Error, "The response did not contain tracking details"
+            message = "The response did not contain tracking details"
+            return TrackingResponse.new(
+              false,
+              message,
+              Hash.from_xml(response),
+              carrier: @@name,
+              xml: response,
+              request: last_request
+            )
           else
             all_unique_identifiers = xml.root.xpath('CompletedTrackDetails/TrackDetails/TrackingNumberUniqueIdentifier').map(&:text)
-            raise ActiveShipping::Error, "Multiple matches were found. Specify a unqiue identifier: #{all_unique_identifiers.join(', ')}"
+            message = "Multiple matches were found. Specify a unqiue identifier: #{all_unique_identifiers.join(', ')}"
+            return TrackingResponse.new(
+              false,
+              message,
+              Hash.from_xml(response),
+              carrier: @@name,
+              xml: response,
+              request: last_request
+            )
         end
-
 
         first_notification = tracking_details.at('Notification')
         severity = first_notification.at('Severity').text
@@ -634,11 +648,11 @@ module ActiveShipping
           end
         end
 
-        if origin_node = tracking_details.at('OriginLocationAddress')
-          origin = Location.new(
-                :country =>     origin_node.at('CountryCode').text,
-                :province =>    origin_node.at('StateOrProvinceCode').text,
-                :city =>        origin_node.at('City').text
+        origin = if origin_node = tracking_details.at('OriginLocationAddress')
+          Location.new(
+            country: origin_node.at('CountryCode').text,
+            province: origin_node.at('StateOrProvinceCode').text,
+            city: origin_node.at('City').text
           )
         end
 
@@ -667,26 +681,29 @@ module ActiveShipping
 
           shipment_events << ShipmentEvent.new(description, zoneless_time, location, description, type_code)
         end
-        shipment_events = shipment_events.sort_by(&:time)
 
+        shipment_events = shipment_events.sort_by(&:time)
       end
 
-      TrackingResponse.new(success, message, Hash.from_xml(response),
-                           :carrier => @@name,
-                           :xml => response,
-                           :request => last_request,
-                           :status => status,
-                           :status_code => status_code,
-                           :status_description => status_description,
-                           :ship_time => ship_time,
-                           :scheduled_delivery_date => scheduled_delivery_time,
-                           :actual_delivery_date => actual_delivery_time,
-                           :delivery_signature => delivery_signature,
-                           :shipment_events => shipment_events,
-                           :shipper_address => (shipper_address.nil? || shipper_address.unknown?) ? nil : shipper_address,
-                           :origin => origin,
-                           :destination => destination,
-                           :tracking_number => tracking_number
+      TrackingResponse.new(
+        success,
+        message,
+        Hash.from_xml(response),
+        carrier: @@name,
+        xml: response,
+        request: last_request,
+        status: status,
+        status_code: status_code,
+        status_description: status_description,
+        ship_time: ship_time,
+        scheduled_delivery_date: scheduled_delivery_time,
+        actual_delivery_date: actual_delivery_time,
+        delivery_signature: delivery_signature,
+        shipment_events: shipment_events,
+        shipper_address: (shipper_address.nil? || shipper_address.unknown?) ? nil : shipper_address,
+        origin: origin,
+        destination: destination,
+        tracking_number: tracking_number
       )
     end
 
--- test/unit/carriers/fedex_test.rb
+++ test/unit/carriers/fedex_test.rb
@@ -318,14 +318,31 @@ class FedExTest < ActiveSupport::TestCase
     @carrier.expects(:commit).returns(mock_response)
 
     assert_raises ActiveShipping::ResponseContentError do
-    @carrier.find_rates(
+      @carrier.find_rates(
         location_fixtures[:ottawa],
         location_fixtures[:beverly_hills],
         package_fixtures.values_at(:book, :wii),
-        :test => true
+        test: true
       )
     end
   end
+
+  def test_parsing_response_without_notifications
+    mock_response = xml_fixture('fedex/reply_without_notifications')
+
+    @carrier.expects(:commit).returns(mock_response)
+
+    response = @carrier.find_rates(
+      location_fixtures[:ottawa],
+      location_fixtures[:beverly_hills],
+      package_fixtures.values_at(:book, :wii),
+      test: true
+    )
+
+    assert_predicate response, :success?
+  end
+
+  ### find_tracking_info
 
   def test_response_transient_failure
     mock_response = xml_fixture('fedex/tracking_response_failure_code_9045')
@@ -350,23 +367,6 @@ class FedExTest < ActiveSupport::TestCase
     msg = 'Sorry, we are unable to process your tracking request.  Please contact Customer Service at 1.800.Go.FedEx(R) 800.463.3339.'
     assert_equal msg, error.message
   end
-
-  def test_parsing_response_without_notifications
-    mock_response = xml_fixture('fedex/reply_without_notifications')
-
-    @carrier.expects(:commit).returns(mock_response)
-
-    response = @carrier.find_rates(
-      location_fixtures[:ottawa],
-      location_fixtures[:beverly_hills],
-      package_fixtures.values_at(:book, :wii),
-      :test => true
-    )
-
-    assert response.success?
-  end
-
-  ### find_tracking_info
 
   def test_tracking_info_for_delivered_with_signature
     mock_response = xml_fixture('fedex/tracking_response_delivered_with_signature')
@@ -507,12 +507,23 @@ class FedExTest < ActiveSupport::TestCase
     mock_response = xml_fixture('fedex/tracking_response_multiple_results')
     @carrier.expects(:commit).returns(mock_response)
 
-    error = assert_raises(ActiveShipping::Error) do
+    error = assert_raises(ActiveShipping::ResponseError) do
       @carrier.find_tracking_info('123456789012')
     end
 
     msg = 'Multiple matches were found. Specify a unqiue identifier: 2456987000~123456789012~FX, 2456979001~123456789012~FX, 2456979000~123456789012~FX'
     assert_equal msg, error.message
+  end
+
+  def test_tracking_info_without_tracking_details
+    mock_response = xml_fixture('fedex/tracking_response_no_results')
+    @carrier.expects(:commit).returns(mock_response)
+
+    error = assert_raises(ActiveShipping::ResponseError) do
+      @carrier.find_tracking_info('abc')
+    end
+
+    assert_equal 'The response did not contain tracking details', error.message
   end
 
   def test_tracking_info_with_unknown_tracking_number
@@ -574,6 +585,8 @@ class FedExTest < ActiveSupport::TestCase
     assert_empty response.shipment_events
   end
 
+  ### create_shipment
+  
   def test_create_shipment
     confirm_response = xml_fixture('fedex/create_shipment_response')
     @carrier.stubs(:commit).returns(confirm_response)
