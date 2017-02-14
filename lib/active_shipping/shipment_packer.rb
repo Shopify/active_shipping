53e06a2a4f282f5d94734e018a8639be0d291c09
Malaz Alamir <malaz.alamir@shopify.com>
2017-02-14 14:44:57 -0500

    Clean up ShipmentPacker#pack

--- lib/active_shipping/shipment_packer.rb
+++ lib/active_shipping/shipment_packer.rb
@@ -11,63 +11,90 @@ module ActiveShipping
     # dimensions      - `[5.0, 15.0, 30.0]`
     # maximum_weight  - maximum weight in grams
     # currency        - ISO currency code
-    def self.pack(items, dimensions, maximum_weight, currency)
-      return [] if items.empty?
-      packages = []
 
-      # Naive in that it assumes weight is equally distributed across all items
-      # Should raise early enough in most cases
-      total_weight = 0
-      items.map!(&:symbolize_keys).each do |item|
-        total_weight += item[:quantity].to_i * item[:grams].to_i
+    class << self
+      def pack(items, dimensions, maximum_weight, currency)
+        return [] if items.empty?
+        packages = []
+        items.map!(&:symbolize_keys)
 
-        if item[:grams].to_i > maximum_weight
-          raise OverweightItem, "The item with weight of #{item[:grams]}g is heavier than the allowable package weight of #{maximum_weight}g"
-        end
+        # Naive in that it assumes weight is equally distributed across all items
+        # Should raise early enough in most cases
+        total_weight = calculate_total_weight(items, maximum_weight)
+        items_to_pack = items.map(&:dup).sort_by! { |i| i[:grams].to_i }
 
-        if total_weight > maximum_weight * EXCESS_PACKAGE_QUANTITY_THRESHOLD
-          raise ExcessPackageQuantity, "Unable to pack more than #{EXCESS_PACKAGE_QUANTITY_THRESHOLD} packages"
-        end
-      end
-
-      items = items.map(&:dup).sort_by! { |i| i[:grams].to_i }
-
-      state = :package_empty
-      while state != :packing_finished
-        case state
-        when :package_empty
-          package_weight, package_value = 0, 0
-          state = :filling_package
-        when :filling_package
-          items.each do |item|
-            quantity = if item[:grams].to_i <= 0
-              item[:quantity].to_i
-            else
-              # Grab the max amount of this item we can fit into this package
-              # Or, if there are fewer than the max for this item, put
-              # what is left into this package
-              [(maximum_weight - package_weight) / item[:grams].to_i, item[:quantity].to_i].min
+        state = :package_empty
+        while state != :packing_finished
+          case state
+          when :package_empty
+            package_weight, package_value = 0, 0
+            state = :filling_package
+          when :filling_package
+            items_to_pack.each do |item|
+              quantity = determine_fillable_quantity_for_package(item, maximum_weight, package_weight)
+              package_weight += item_weight(quantity, item[:grams])
+              package_value += item_value(quantity, item[:price])
+              item[:quantity] = item[:quantity].to_i - quantity
             end
 
-            item_weight = quantity * item[:grams].to_i
-            item_value = quantity * Package.cents_from(item[:price])
+            items_to_pack.reject! { |i| i[:quantity].to_i == 0 }
+            state = :package_full
+          when :package_full
+            packages << ActiveShipping::Package.new(package_weight, dimensions, value: package_value, currency: currency)
+            state = items_to_pack.any? ? :package_empty : :packing_finished
+          end
+        end
 
-            package_weight += item_weight
-            package_value += item_value
+        packages
+      end
 
-            item[:quantity] = item[:quantity].to_i - quantity
+      private
+
+      def calculate_total_weight(items, maximum_weight)
+        items.inject(0) do |total_weight, item|
+          total_weight += item[:quantity].to_i * item[:grams].to_i
+
+          if overweight_item?(item[:grams], maximum_weight)
+            raise OverweightItem, "The item with weight of #{item[:grams]}g is heavier than the allowable package weight of #{maximum_weight}g"
           end
 
-          items.reject! { |i| i[:quantity].to_i == 0 }
+          if excess_package_quantity?(total_weight, maximum_weight)
+            raise ExcessPackageQuantity, "Unable to pack more than #{EXCESS_PACKAGE_QUANTITY_THRESHOLD} packages"
+          end
 
-          state = :package_full
-        when :package_full
-          packages << ActiveShipping::Package.new(package_weight, dimensions, :value => package_value, :currency => currency)
-          state = items.any? ? :package_empty : :packing_finished
+          total_weight
         end
       end
 
-      packages
+      def overweight_item?(grams, maximum_weight)
+        grams.to_i > maximum_weight
+      end
+
+      def excess_package_quantity?(total_weight, maximum_weight)
+        total_weight > (maximum_weight * EXCESS_PACKAGE_QUANTITY_THRESHOLD)
+      end
+
+      def determine_fillable_quantity_for_package(item, maximum_weight, package_weight)
+        item_grams = item[:grams].to_i
+        item_quantity = item[:quantity].to_i
+
+        if item_grams <= 0
+          item_quantity
+        else
+          # Grab the max amount of this item we can fit into this package
+          # Or, if there are fewer than the max for this item, put
+          # what is left into this package
+          [(maximum_weight - package_weight) / item_grams, item_quantity].min
+        end
+      end
+
+      def item_weight(quantity, grams)
+        quantity * grams.to_i
+      end
+
+      def item_value(quantity, price)
+        quantity * Package.cents_from(price)
+      end
     end
   end
 end
